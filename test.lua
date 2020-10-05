e569f43a403fb211367ece54f01bbee4d7eb9455
weifc <root@Ubuntu16.4>
2020-10-05 23:56:18 +0800

    Lua 元表(Metatable)

--- test.lua
+++ test.lua
@@ -625,7 +625,27 @@ print("tb1 长度: ", #tb1)
 Lua 模块与包
 --]]
 
+-- 文件名为 module.lua 
+-- 定义一个名为 module 的模块
+module = {}
 
+-- 定义一个常量
+module.constant = "这是一个常量"
+
+-- 定义一个函数
+function module.func1()
+    io.write("这是一个共有函数! \n")
+end
+
+local function func2()
+    print("这是一私有函数! ")
+end
+
+function module.func3()
+    func2()
+end
+
+return module
 
 
 
---
+++ test_module.lua
@@ -0,0 +1,9 @@
+-- test_module.lua 文件 
+-- modult 模块为上文提到 module.lua 
+
+require("module")
+
+print(module.constant)
+
+module.func3()
+
