5b93ee8e22a3c812b1222832b8f837bb8c5d1c31
weifc <root@Ubuntu16.4>
2020-10-12 17:36:12 +0800

    Lua 协同

--- test.lua
+++ test.lua
@@ -1001,10 +1001,6 @@ function Shape:new (o, side)
     self.
 end
 
-
---]]
--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
 -- 元类
 Rectangle = { area = 0, length = 0, breadth = 0 }
 
@@ -1025,8 +1021,73 @@ function Rectangle : printArea()
 end
 
 
+function getSum(num1, num2)
+    return num1 + num2 
+end
+
+print(string.format("5 + 2 = %d", getSum(5, 2)))
+
+function splitStr(theString)
+    stringTable = {}
+
+    local i = 1
+
+    for word in string.gmatch(theString, "[^%s]+") do
+        stringTable[i] = word
+        i = i + 1
+    end
+    
+    return stringTable, i
+end
+
+splitStrTable, numOfStr = splitStr("The Turtle")
+
+for j = 1, numOfStr do
+    print(string.format("%d : %s", j, splitStrTable[j]))
+end
+
+function getSumMore(...)
+    local sum = 0
+
+    for k, v in pairs{...} do
+        sum = sum + v
+    end
+    return sum 
+end
+
+io.write("Sum ", getSumMore(1,2,3,4,5,6), "\n")
+
+doubleIt = function(x) return x * 2 end 
+
+print(doubleIt(4))
+
+function outerFunc()
+    local i = 0
+
+    return function()
+        i = i + 1
+        return i
+    end 
+end
+
+getI = outerFunc()
+
+print(getI())
+print(getI())
 
 
+--]]
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+co = coroutine.create(function()
+    for i = 1, 10, 1 do
+        print(i)
+        print(coroutine.status(co))
+        if i == 5 then coroutine.yield() end
+    end
+end)
+
+print(coroutine.status(co))
 
 
 
